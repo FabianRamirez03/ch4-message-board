d37f34211ed473350d0a8ebb4b827a02f230c3e2
FabianRamirez03 <ramirezfabian003@gmail.com>
2023-01-22 19:57:42 -0600

    Fixed allowed Hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-pxjg-hn-$bvf=yw28y58qigm65-_8)uk*z-)htt=jpz!#)@n50
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
